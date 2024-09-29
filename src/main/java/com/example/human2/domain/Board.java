ea8118a1dcc2ad7f6bad7ab3a63da2de9043b09e
leegunwoooo <leegunwoo0325@gmail.com>
2024-09-30 08:39:35 +0900

    기본 crud

--- src/main/java/com/example/human2/domain/Board.java
+++ src/main/java/com/example/human2/domain/Board.java
@@ -1,4 +1,29 @@
 package com.example.human2.domain;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "2human")
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    @Builder
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
