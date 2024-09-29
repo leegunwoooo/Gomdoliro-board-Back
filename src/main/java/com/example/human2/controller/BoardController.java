7353bb5c6d65905de8e0dd1d9992d0a230345240
leegunwoooo <leegunwoo0325@gmail.com>
2024-09-30 08:39:41 +0900

    기본 crud

--- src/main/java/com/example/human2/controller/BoardController.java
+++ src/main/java/com/example/human2/controller/BoardController.java
@@ -1,9 +1,42 @@
 package com.example.human2.controller;
 
+import com.example.human2.controller.dta.BoardResponse;
+import com.example.human2.controller.dta.SaveBoardRequest;
+import com.example.human2.controller.dta.UpdateBoardRequest;
+import com.example.human2.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/board")
+    public BoardResponse save(@RequestBody SaveBoardRequest request) {
+        return boardService.save(request);
+    }
+
+    @GetMapping("/board-all")
+    public List<BoardResponse> getAll() {
+        return boardService.findAll();
+    }
+
+    @GetMapping("/board/{id}")
+    public BoardResponse findOne(@PathVariable Long id) {
+        return boardService.findOne(id);
+    }
+
+    @PutMapping("/board")
+    public BoardResponse update(@RequestBody UpdateBoardRequest request) {
+        return boardService.update(request);
+    }
+
+    @DeleteMapping("/board/{id}")
+    public void delete(@PathVariable Long id) {
+        boardService.delete(id);
+    }
 }
--- src/main/java/com/example/human2/controller/dta/BoardRespones.java
+++
@@ -1,4 +0,0 @@
-package com.example.human2.controller.dta;
-
-public class BoardRespones {
-}
