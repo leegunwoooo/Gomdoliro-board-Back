69f31d34552ce35c992e7aa3c0bfbd3a61dad6b6
leegunwoooo <leegunwoo0325@gmail.com>
2024-09-30 08:39:54 +0900

    기본 crud

--- src/main/java/com/example/human2/controller/dta/SaveBoardRequest.java
+++ src/main/java/com/example/human2/controller/dta/SaveBoardRequest.java
@@ -1,4 +1,11 @@
 package com.example.human2.controller.dta;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class SaveBoardRequest {
+    private String title;
+    private String content;
 }
--- src/main/java/com/example/human2/controller/dta/UpdateBoardRequest.java
+++ src/main/java/com/example/human2/controller/dta/UpdateBoardRequest.java
@@ -1,4 +1,12 @@
 package com.example.human2.controller.dta;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UpdateBoardRequest {
+    private Long id;
+    private String title;
+    private String content;
 }
