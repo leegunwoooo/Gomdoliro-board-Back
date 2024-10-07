57862239c1cdec602ef5f7aa37577f45c689f2ae
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-07 15:18:43 +0900

    게시물 생성시간 코드

--- src/main/java/com/example/human2/controller/dto/BoardResponse.java
+++ src/main/java/com/example/human2/controller/dto/BoardResponse.java
@@ -4,6 +4,8 @@ import com.example.human2.domain.Board;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 
 @Getter
 @AllArgsConstructor
@@ -11,10 +13,12 @@ public class BoardResponse {
     private Long id;
     private String title;
     private String content;
+    private LocalDate createDate;
 
     public BoardResponse(Board board) {
         this.id = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
+        this.createDate = board.getCreateDate();
     }
 }
--- src/main/java/com/example/human2/domain/Board.java
+++ src/main/java/com/example/human2/domain/Board.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
@@ -18,6 +22,10 @@ public class Board {
 
     @Column(length = 50000)
     private String content;
+
+    @CreatedDate//엔티티가 생성될 때 자동으로 생성시간을 기록하는 어노테이션
+    @Column(updatable = false)//한번 올라가면 수정안되게
+    private LocalDate createDate; //LocalDate를 이용해 날짜를 저장
 
     @Builder
     public Board(String title, String content) {
