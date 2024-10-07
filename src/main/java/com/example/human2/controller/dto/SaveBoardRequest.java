36ac7e02b35ea64d686091e529e6606a16dcfe0d
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-07 15:26:37 +0900

    게시물 생성시간 코드

--- src/main/java/com/example/human2/controller/dto/SaveBoardRequest.java
+++ src/main/java/com/example/human2/controller/dto/SaveBoardRequest.java
@@ -3,9 +3,13 @@ import com.example.human2.domain.Board;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class SaveBoardRequest {
     private String title;
     private String content;
+    private LocalDate createDate;
 }
