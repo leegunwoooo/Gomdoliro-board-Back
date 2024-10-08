486317fbfb222f285b901486e39c5052af2ce08f
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-08 09:22:13 +0900

    로그인, 회원가입 수정

--- src/main/java/com/example/human2/config/WebSecurityConfig.java
+++ src/main/java/com/example/human2/config/WebSecurityConfig.java
@@ -1,11 +1,8 @@
 package com.example.human2.config;
 
-import com.example.human2.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,19 +14,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
 
-    private final UserService userService;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize // 권한 설정
-                        .requestMatchers("/login", "/signup", "/user", "board-all").permitAll() // 누구나 접근 허용
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login", "/signup", "/board-all").permitAll() // 로그인과 회원가입은 누구나 접근 가능
+                        .requestMatchers("/board", "/board/").authenticated() // 특정 페이지는 인증 필요
+                        .anyRequest().permitAll() // 나머지 페이지는 인증 없이 접근 가능
                 )
-                .formLogin((formLogin) -> formLogin // 폼 기반 로그인 설정
+                .formLogin((formLogin) -> formLogin
                         .loginPage("/login") // 사용자 정의 로그인 페이지
-                        .defaultSuccessUrl("/board-all") // 로그인 성공 시 이동할 페이지
+                        .defaultSuccessUrl("/board-all", true) // 로그인 성공 시 이동할 페이지
                 )
-                .logout((logout) -> logout // 로그아웃 설정
+                .logout((logout) -> logout
                         .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동할 페이지
                         .invalidateHttpSession(true) // 세션 무효화
                 );
@@ -40,10 +37,5 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // 비밀번호 암호화
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
