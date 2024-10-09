23f1c1030722845baea29bf29287fd0771d190e5
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-09 23:07:16 +0900

    작성자명 보이기

--- src/main/java/com/example/human2/domain/Board.java
+++ src/main/java/com/example/human2/domain/Board.java
@@ -25,14 +25,17 @@ public class Board {
     @Column(length = 50000)
     private String content;
 
+    private String nickname;
+
     @CreatedDate//엔티티가 생성될 때 자동으로 생성시간을 기록하는 어노테이션
     @Column(updatable = false)//한번 올라가면 수정안되게
     private LocalDate date; //LocalDate를 이용해 날짜를 저장
 
     @Builder
-    public Board(String title, String content) {
+    public Board(String title, String content, String nickname) {
         this.title = title;
         this.content = content;
+        this.nickname = nickname;
     }
 
     public void update(String title, String content) {
--- src/main/java/com/example/human2/service/BoardService.java
+++ src/main/java/com/example/human2/service/BoardService.java
@@ -5,9 +5,13 @@ import com.example.human2.controller.dto.SaveBoardRequest;
 import com.example.human2.controller.dto.UpdateBoardRequest;
 import com.example.human2.domain.Board;
 import com.example.human2.domain.BoardRepository;
+import com.example.human2.domain.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import com.example.human2.domain.UserRepository;
+
 
 import java.util.List;
 
@@ -16,11 +20,17 @@ import java.util.List;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
 
     public BoardResponse save(SaveBoardRequest request) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName(); // 로그인한 사용자의 이메일 가져오기
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found")); // 사용자 정보 가져오기
+
         Board board = Board.builder()
                 .title(request.getTitle())
                 .content(request.getContent())
+                .nickname(user.getNickName()) // 닉네임 설정
                 .build();
 
         Board save = boardRepository.save(board);
