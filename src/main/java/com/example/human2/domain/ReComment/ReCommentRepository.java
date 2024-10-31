d5752b405dcfb50083aa8193f478ca0aa7dc5a37
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-31 20:12:06 +0900

    fix : optinal로 반환하고 있던걸 list로 변경

--- src/main/java/com/example/human2/domain/ReComment/ReCommentRepository.java
+++ src/main/java/com/example/human2/domain/ReComment/ReCommentRepository.java
@@ -3,8 +3,9 @@ package com.example.human2.domain.ReComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReCommentRepository extends JpaRepository<ReComment, Long> {
-    Optional<ReComment> findByCommentId(Long commentId);
+    List<ReComment> findByCommentId(Long commentId);
 }
