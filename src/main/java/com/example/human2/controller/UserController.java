5799a73a972094202bcf1d6e723b24f7b111d1f7
leegunwoooo <leegunwoo0325@gmail.com>
2024-10-31 19:39:47 +0900

    fix : db create로

--- src/main/java/com/example/human2/controller/UserController.java
+++ src/main/java/com/example/human2/controller/UserController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication; // Spring Security의 Authentication 사용
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
