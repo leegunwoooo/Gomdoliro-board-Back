8c4ea60a6737de2957011f9b7bfa6765acd434ab
leegunwoooo <leegunwoo0325@gmail.com>
2024-11-06 16:36:40 +0900

    Feat : 대댓글 생성시간 추가

--- src/main/java/com/example/human2/controller/dto/ReComment/ReCommentResponse.java
+++ src/main/java/com/example/human2/controller/dto/ReComment/ReCommentResponse.java
@@ -4,6 +4,8 @@ import com.example.human2.domain.ReComment.ReComment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 @AllArgsConstructor
 public class ReCommentResponse {
@@ -11,12 +13,14 @@ public class ReCommentResponse {
     private String reCommentContent;
     private Long commentId;
     private String reCommentWriter;
+    private LocalDate reCommentDate;
 
     public ReCommentResponse(ReComment reComment) {
         this.id = reComment.getId();
         this.commentId = reComment.getComment().getId();
         this.reCommentContent = reComment.getReCommentContent();
         this.reCommentWriter = reComment.getReCommentWriter();
+        this.reCommentDate = reComment.getReCommentDate();
 
     }
 }
