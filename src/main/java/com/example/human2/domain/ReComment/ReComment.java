6bf5207bd03369579de4c672c0c556b441f6fbeb
leegunwoooo <leegunwoo0325@gmail.com>
2024-11-06 16:34:55 +0900

    Feat : 대댓글 생성시간 추가

--- src/main/java/com/example/human2/domain/ReComment/ReComment.java
+++ src/main/java/com/example/human2/domain/ReComment/ReComment.java
@@ -5,7 +5,10 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
@@ -25,6 +28,10 @@ public class ReComment {
     private Comment comment;
 
     private String reCommentWriter;
+
+    @CreatedDate//엔티티가 생성될 때 자동으로 생성시간을 기록하는 어노테이션
+    @Column(updatable = false)//한번 올라가면 수정안되게
+    private LocalDate date; //LocalDate를 이용해 날짜를 저장
 
     @Builder
     public ReComment(String reCommentContent, Comment comment, String reCommentWriter){
